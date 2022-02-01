ac1f10d654a795e92d1bfc32818d86875cc633d2
nikolai <Nikolai Goncharov>
2022-02-01 22:18:00 +0300

    Better run

---
+++ run_benchmark.sh
@@ -0,0 +1,14 @@
+python benchmark_semantic.py \
+--dataset 'kitti' \
+--model-name 'dsnet' \
+--batch-size 2 \
+--n-steps 500 \
+--n-classes 2 \
+--train-size 0.75 \
+--device-name 'cuda:0' \
+--scene-size 120000 \
+--dataset 'kitti' \
+--model-name 'dsnet' \
+--model-state-path '/mnt/vol0/datasets/plane_extraction_model_states/selected_models/dsnet_semantic_full_kitti.pth' \
+--train \
+--val
