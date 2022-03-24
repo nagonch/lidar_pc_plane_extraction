8f2eb0cd8af689d5a0a0059b79e4dd41cc01e7a0
nikolai <Nikolai Goncharov>
2022-03-24 20:03:24 +0300

    fix

--- gps3_net/utils.py
+++ gps3_net/utils.py
@@ -37,15 +37,16 @@ def get_gt_edges(gt_labels, overseg_labels):
     return edges
 
 
-def convert_to_net_data(batch, cluster_labels, spatial_shape=[480, 360, 32]):
+def convert_to_net_data(batch, clusterer, spatial_shape=[480, 360, 32]):
     gt_labels = batch['pt_labs'][0].reshape(-1)
     mask = gt_labels >= 1
     grid = batch['grid'][0][mask]
     
     pt_fea = batch['pt_fea'][0][mask]
     xyz = batch['xyz'][0][mask]
+    cluster_labels = clusterer.fit_predict(xyz)
     
-    features = torch.cat((batch['xyz'][0], torch.tensor(pt_fea)), axis=1)
+    features = torch.cat((xyz, torch.tensor(pt_fea)), axis=1)
     indices = torch.cat((cluster_labels[None].T, torch.tensor(grid)), axis=1)
     spatial_shape = torch.tensor(spatial_shape)
     
@@ -53,4 +54,3 @@ def convert_to_net_data(batch, cluster_labels, spatial_shape=[480, 360, 32]):
                                   cluster_labels, out=torch.zeros_like(xyz), dim=0)[:cluster_labels.max() + 1, :]
     
     return xyz, features, indices, spatial_shape, gt_labels, node_centroids, batch['vox_coor'][0]
-
