e44b3114b837c873a0b153235148672bf06a8f80
nikolai <Nikolai Goncharov>
2022-03-30 01:57:55 +0300

    fix

--- train_gps3net.py
+++ train_gps3net.py
@@ -75,6 +75,6 @@ for step in range(N_STEPS):
                 'epoch': step,
                 'model_state': model.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict(),
-                'loss': torch.mean(loss),
+                'loss': torch.mean(loss).item(),
             }, model_save_path.format(RUN_ID),
     )
